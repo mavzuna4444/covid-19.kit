282cb82b1f5dc9a9a0491a0160b3abc2f70aefbd
mavzuna4444 <162799817+mavzuna4444@users.noreply.github.com>
2024-12-22 18:07:02 +0500

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import BaggingClassifier
 
+st.title('Prediction of COVID-19')
+with st.expander('Data'):
+  data = pd.read_csv("covid-19 symptoms dataset.csv")
